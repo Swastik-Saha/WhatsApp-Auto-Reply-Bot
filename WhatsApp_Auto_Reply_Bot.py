35c804ee39a5f7e63dba23c9dcf3d08fb7564876
Swastik Saha <65535891+Swastik-Saha@users.noreply.github.com>
2020-08-11 20:28:13 +0530

    Update WhatsApp_Auto_Reply_Bot.py

--- WhatsApp_Auto_Reply_Bot.py
+++ WhatsApp_Auto_Reply_Bot.py
@@ -17,7 +17,6 @@ def locate_reply_box():
 while True:
     time.sleep(1)
     rx, ry = locate_reply_box()
-    region_to_scan = (rx-62, ry-16, 70, -30)
 
     for x in range(rx-62, rx+9):
         for y in range(ry-16, ry-47, -1):
@@ -26,6 +25,7 @@ while True:
                 pyautogui.click(rx, ry+24, button='left')
                 pyautogui.typewrite("Nice to meet you. Swastik will contact you soon.")
                 pyautogui.press("enter")
+                
                 time.sleep(2)
 
     
